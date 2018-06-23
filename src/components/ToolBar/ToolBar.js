3d80901c3303fbc880d50419468299b6836b62c1
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-06-23 12:06:16 +0300

    MOBILE-3. Added logic for switching timer modes

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import s from './ToolBar.styles';
 
-const ToolBar = ({ children }) => {
+const ToolBar = ({ children, isHidden = false }) => {
+  const containerStyles = [s.container];
+  const pointerEvents = isHidden ? { pointerEvents: 'none' } : {};
+
+  if (isHidden) {
+    containerStyles.push(s.containerHidden);
+  }
+
   return (
-    <View style={s.container}>
+    <View {...pointerEvents} style={containerStyles}>
       {children}
     </View>
   );
---
+++ src/config/texts.js
@@ -0,0 +1,22 @@
+export default {
+  modes: {
+    focus: {
+      start: 'Start focused work!',
+      process: 'Do as much as you can',
+    },
+    shortBreak: {
+      start: 'Start short break',
+      process: 'Take a short break',
+    },
+    longBreak: {
+      start: 'Start long break',
+      process: 'Relax, you deserve it!',
+    },
+    stopWatch: {
+      start: 'Start measure length of your session',
+      process: 'Stopwatch in progress',
+      finish: 'Finished. Take a break...'
+    },
+    cancelTimer: 'Aborted. IT happens, don\'t worry and try it again!',
+  },
+};
